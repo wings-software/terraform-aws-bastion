99bdda39def1becd2d3cc38e2279f851372192ec
Dave Arnold <darnold@hashicorp.com>
2018-06-21 18:00:20 -0700

    adding public_subnet id to sec group name in order to avoid conflicts

--- sec_groups.tf
+++ sec_groups.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "bastion" {
-  name   = "${var.network_name}-bastion"
+  name   = "${var.network_name}-${var.public_subnet_id}-bastion"
   vpc_id = "${var.vpc_id}"
 
   ingress {
