a1c0261a7f6d04b1cc4395ae636bf610c701deea
Dave Arnold <dave.arnold@harness.io>
2019-02-25 10:20:42 -0800

    Update sec_groups.tf

--- sec_groups.tf
+++ sec_groups.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "bastion" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${var.ssh_access}"]
+    cidr_blocks = "${var.ssh_access}"
   }
 
   egress {
